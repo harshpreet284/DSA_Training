54b60caf6f6f2ea07579b55ceccf20dd0ca3f84c
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-06-17 09:35:45 +0530

    add

---
+++ DAY 1/a.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ABC{
+    public static void main(String[] args) {
+    int a = 5;
+    int b = 6;
+    System.out.println("Value of a :" + a);
+    System.out.println("Value of b :" + b);
+     a = a+b;
+     b = a-b;
+     b = a+b;
+    System.out.println("Value of a :" + a);   
+}
+}
